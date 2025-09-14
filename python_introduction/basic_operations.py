c3758b5c4d89812cdcf33301e732ac63c391e486
Leul Misganaw <leulmisganaw222@gmail.com>
2025-09-14 07:36:18 -0400

    Re-add python_introduction files normally

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+add = number1 + number2
+sub = number1 - number2
+multi = number1 * number2
+
+print(f"Addition of 10 and 5 is {add}")
+print(f"Subtraction of 10 and 5 is {sub}")
+print(f"Multiplication of 10 and 5 is {multi}")
